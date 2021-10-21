39dd4547a2933fe37c50b46a14fc3d3fa45b1257
havadtoiGabriella <lia.havadtoigabriella@gmail.com>
2021-10-21 16:51:02 +0200

    deleting files

--- week-02/day-04/Colors.java
+++ week-02/day-04/Colors.java
@@ -9,7 +9,8 @@ public class Colors {
 // - A `colors[2]` pedig a pink árnyalatait:
 //   `"orchid", "violet", "pink", "hot pink"`
 
-        String[][] colors = new String[3][5];
+        String colors = {{"lime", "forest green", "olive", "pale green", "spring green"}, {"orange red", "red", "tomato"}, {"orchid", "violet", "pink", "hot pink"}};
+       /* String[][] colors = new String[3][5];
         colors[0][0] = "lime, ";
         colors[0][1] = "forest green, ";
         colors[0][2] = "olive, ";
@@ -27,7 +28,7 @@ public class Colors {
         colors[2][2] = "pink, ";
         colors[2][3] = "hot pink, ";
         colors[2][4] = " ";
-
+*/
         for (int i = 0; i < colors.length; i++) {
             for (int j = 0; j < colors[i].length; j++) {
                 System.out.print(colors[i][j] + " ");
