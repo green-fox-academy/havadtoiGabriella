51bef480f444e318af6703cb3794c64b591850ff
havadtoiGabriella <lia.havadtoigabriella@gmail.com>
2022-04-13 16:48:52 +0200

    Swap added

---
+++ auto-tester/week01/expressions-and-controlflow/src/com/gfa/havadtoigabriella/Swap.java
@@ -0,0 +1,16 @@
+package com.gfa.havadtoigabriella;
+
+public class Swap {
+  public static void main(String[] args) {
+    // Swap the values of the variables
+    int a = 123;
+    int b = 526;
+    int c = a;
+
+    a = b;
+    b = c;
+
+    System.out.println(a);
+    System.out.println(b);
+  }
+}
