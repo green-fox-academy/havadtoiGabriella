7c08029fda532d6f94ceb31fc7d2d7ff2ccc9429
havadtoiGabriella <lia.havadtoigabriella@gmail.com>
2022-04-13 16:23:07 +0200

    two more added

--- auto-tester/week01/expressions-and-controlflow/src/HelloMe.java
+++ auto-tester/week01/expressions-and-controlflow/src/HelloMe.java
@@ -2,6 +2,6 @@ public class HelloMe {
   public static void main(String[] args) {
     // Úgy módosítsd a programot, hogy téged üdvözöljön a Világ helyett
     String name = "Gabi";
-    System.out.println("Hello "+ name+"!");
+    System.out.println("Hello " + name + "!");
   }
 }
---
+++ auto-tester/week01/expressions-and-controlflow/src/com/gfa/havadtoigabriella/HelloOthers.java
@@ -0,0 +1,12 @@
+package com.gfa.havadtoigabriella;
+
+public class HelloOthers {
+  public static void main(String[] args) {
+    // Üdvözöld 3 osztálytársad 3 sorban kiiratva a nevüket, ehhez hasonlóan:
+    //
+    // Helló Eszter!
+    // Helló Maris!
+    // Helló Józsi!
+    System.out.println("Hello Mónika!\nHello Eszter!\nHello Szilvi!");
+  }
+}
