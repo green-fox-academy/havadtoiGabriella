94b776a87572d01498b5aa8cfcb1b89e2573c70c
havadtoiGabriella <lia.havadtoigabriella@gmail.com>
2022-04-13 18:25:32 +0200

    BasicInfo added

---
+++ auto-tester/week01/expressions-and-controlflow/src/com/gfa/havadtoigabriella/DefineBasicInfo.java
@@ -0,0 +1,19 @@
+package com.gfa.havadtoigabriella;
+
+public class DefineBasicInfo {
+  public static void main(String[] args) {
+    // Define several things as a variable then print their values
+    // Your name as a string
+    // Your age as an integer
+    // Your height in meters as a double
+    // Whether you are married or not as a boolean
+    String myName = "Havadtoi Gabriella";
+    int myAge = 29;
+    double myHeight = 1.65;
+    boolean isMarried = false;
+
+    System.out.println("My name is "+ myName+", I'm "+ myAge+" years old, my height is "+myHeight+" meter and I'm " + (isMarried ? "married" : "not married."));
+
+  }
+}
+
