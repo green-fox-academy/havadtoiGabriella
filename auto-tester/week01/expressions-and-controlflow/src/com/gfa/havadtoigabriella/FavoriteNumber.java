32ae1df73b8dcbd2ed93d5a960e4d0911493bfb4
havadtoiGabriella <lia.havadtoigabriella@gmail.com>
2022-04-13 16:48:34 +0200

    FavoriteNumber added

---
+++ auto-tester/week01/expressions-and-controlflow/src/com/gfa/havadtoigabriella/FavoriteNumber.java
@@ -0,0 +1,10 @@
+package com.gfa.havadtoigabriella;
+
+public class FavoriteNumber {
+  public static void main(String[] args) {
+    int favoriteNumber = 8;
+    // Store your favorite number in a variable (as a number)
+    // And print it like this: "My favorite number is: 8"
+    System.out.println("My favorite number is: "+ favoriteNumber);
+  }
+}
