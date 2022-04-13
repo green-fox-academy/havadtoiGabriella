db71f4b11dfac57deebb754d6cae68370fe71fb1
havadtoiGabriella <lia.havadtoigabriella@gmail.com>
2022-04-13 14:11:52 +0200

    first 6 exercises added

---
+++ auto-tester/expressions-and-control-flow/src/com/gfa/havadtoigabriella/HelloMe.java
@@ -0,0 +1,10 @@
+package com.gfa.havadtoigabriella;
+
+public class HelloMe {
+  public static void main(String[] args) {
+    // Modify this program to greet you instead of the World!
+    String myName = "Gabi";
+    System.out.println("Hello, "+ myName+ "!");
+  }
+}
+
