6a02beef61076cc3db62a80fe7f1cc5ad10c92f3
havadtoiGabriella <lia.havadtoigabriella@gmail.com>
2022-04-13 16:17:58 +0200

    HelloMe added

---
+++ auto-tester/week01/expressions-and-controlflow/src/HelloMe.java
@@ -0,0 +1,7 @@
+public class HelloMe {
+  public static void main(String[] args) {
+    // Úgy módosítsd a programot, hogy téged üdvözöljön a Világ helyett
+    String name = "Gabi";
+    System.out.println("Hello "+ name+"!");
+  }
+}
