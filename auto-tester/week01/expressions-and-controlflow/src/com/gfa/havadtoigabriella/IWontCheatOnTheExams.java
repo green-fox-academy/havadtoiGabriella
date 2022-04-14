f867797069e5dee8d01ee2e6dbac85dc19e7cb20
havadtoiGabriella <lia.havadtoigabriella@gmail.com>
2022-04-14 15:41:50 +0200

    IWontCheat added

---
+++ auto-tester/week01/expressions-and-controlflow/src/com/gfa/havadtoigabriella/IWontCheatOnTheExams.java
@@ -0,0 +1,11 @@
+package com.gfa.havadtoigabriella;
+
+public class IWontCheatOnTheExams {
+  public static void main(String[] args) {
+    // Create a program that writes this line 100 times:
+    // "I won't cheat on the exam!"
+    for (int i = 0; i < 100; i++) {
+      System.out.println("I won't cheat on the exam!");
+    }
+  }
+}
