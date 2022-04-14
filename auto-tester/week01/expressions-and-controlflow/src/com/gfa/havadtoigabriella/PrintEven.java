cbec192abd5767a4c66aaf53d2f3f9645de7702b
havadtoiGabriella <lia.havadtoigabriella@gmail.com>
2022-04-14 15:43:11 +0200

    PrintEven added

---
+++ auto-tester/week01/expressions-and-controlflow/src/com/gfa/havadtoigabriella/PrintEven.java
@@ -0,0 +1,10 @@
+package com.gfa.havadtoigabriella;
+
+public class PrintEven {
+  // Create a program that prints all the even numbers between 0 and 500
+  public static void main(String[] args) {
+    for (int i = 0; i < 501; i += 2) {
+      System.out.println(i);
+    }
+  }
+}
