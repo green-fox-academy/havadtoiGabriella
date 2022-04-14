6cfd778064fe2f6abba005bd78d1ad79801e0891
havadtoiGabriella <lia.havadtoigabriella@gmail.com>
2022-04-14 16:47:51 +0200

    r

--- manual-tester/week-02/day-02-03/DrawDiamond.java
+++ manual-tester/week-02/day-02-03/DrawDiamond.java
@@ -1,41 +1,37 @@
+package com.gfa.havadtoigabriella;
+
 import java.util.Scanner;
 
 public class DrawDiamond {
-    public static void main(String[] args) {
-        // Írj egy programot, ami egy számot kér a standard input-ból és egy
-// ilyen rombuszt rajzol:
-//
-//
-//    *
-//   ***
-//  *****
-// *******
-//  *****
-//   ***
-//    *
-//
-// A rombusznak annyi sora legyen,
-// mint amennyi az inputból kiolvasott szám értéke
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Type in a number, how big the diamond should be!");
-        int lineCount = scanner.nextInt();
-        for (int i = 1; i < lineCount; i += 2){
-            for (int j = 0; j < lineCount - 1 - i / 2; j++){
-                System.out.print(" ");
-            }
-            for (int j = 0; j < i; j++){
-                System.out.print("*");
-            }
-            System.out.print("\n");
-        }
-        for (int i = lineCount; i > 0; i -= 2){
-            for (int j = 0; j < lineCount - 1 - i / 2; j++) {
-                System.out.print(" ");
-            }
-            for (int j = 0; j < i; j++) {
-                System.out.print("*");
-            }
-            System.out.print("\n");
-        }
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Type in a number, how big the diamond should be!");
+    int sizeOfDiamond = scanner.nextInt();
+    int lineCount;
+    if (sizeOfDiamond % 2 != 0) {
+      lineCount = sizeOfDiamond / 2 + 1;
+    } else {
+      lineCount = sizeOfDiamond / 2;
     }
+    for (int i = 1; i <= lineCount; i++) {
+      for (int j = 0; j < lineCount - i; j++) {
+        System.out.print(" ");
+      }
+      for (int j = 0; j < i * 2 - 1; j++) {
+        System.out.print("*");
+      }
+      System.out.print("\n");
+    }
+    for (int i = lineCount - 1; i >= 1; i--) {
+      for (int j = 0; j < lineCount - i; j++) {
+        System.out.print(" ");
+      }
+      for (int j = 0; j < i * 2 - 1; j++) {
+        System.out.print("*");
+      }
+      System.out.print("\n");
+    }
+  }
 }
+
