14bbd5055eb06fbeccdb301e817acab3564e9189
havadtoiGabriella <lia.havadtoigabriella@gmail.com>
2022-04-14 15:41:23 +0200

    FizzBuzz added

---
+++ auto-tester/week01/expressions-and-controlflow/src/com/gfa/havadtoigabriella/FizzBuzz.java
@@ -0,0 +1,26 @@
+package com.gfa.havadtoigabriella;
+
+public class FizzBuzz {
+  public static void main(String[] args) {
+    fizzBuzz();
+    // Write a program that prints the numbers from 1 to 100
+// but for multiples of three print “Fizz” instead of the number
+// and for the multiples of five print “Buzz”.
+// For numbers which are multiples of both three and five print “FizzBuzz”.
+  }
+
+  public static void fizzBuzz() {
+    for (int i = 1; i < 101; i++) {
+      if (i % 3 == 0 && i % 5 == 0) {
+        System.out.println("FizzBuzz");
+      } else if (i % 3 == 0) {
+        System.out.println("Fizz");
+      } else if (i % 5 == 0) {
+        System.out.println("Buzz");
+      } else {
+        System.out.println(i);
+      }
+    }
+  }
+}
+
