ddda05562ba093f924a919c58ccbc6116b20284b
havadtoiGabriella <lia.havadtoigabriella@gmail.com>
2022-04-18 12:32:54 +0200

    Increment element added

---
+++ auto-tester/week02/arrays/src/com/gfa/havadtoigabriella/IncrementElement.java
@@ -0,0 +1,13 @@
+package com.gfa.havadtoigabriella;
+
+public class IncrementElement {
+  public static void main(String[] args) {
+    // - Create an array variable named `numbers`
+//   with the following content: `[1, 2, 3, 4, 5]`
+// - Increment the third element
+// - Print the third element
+    int[] numbers = {1, 2, 3, 4, 5};
+    numbers[2]++;
+    System.out.println(numbers[2]);
+  }
+}
