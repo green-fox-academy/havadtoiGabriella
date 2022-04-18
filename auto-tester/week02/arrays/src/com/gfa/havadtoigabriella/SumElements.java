ec8aa1e0b801d4cb59398feb9938bf036d2d2cf0
havadtoiGabriella <lia.havadtoigabriella@gmail.com>
2022-04-18 12:29:07 +0200

    Sum elements added

---
+++ auto-tester/week02/arrays/src/com/gfa/havadtoigabriella/SumElements.java
@@ -0,0 +1,12 @@
+package com.gfa.havadtoigabriella;
+
+public class SumElements {
+  // - Create an array variable named `numbers`
+//   with the following content: `[54, 23, 66, 12]`
+// - Print the sum of the second and the third element
+  public static void main(String[] args) {
+    int[] numbers = {54, 23, 66, 12};
+    System.out.println(
+        "The sum of the second and the third element is: " + (numbers[1] + numbers[2]));
+  }
+}
