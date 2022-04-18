b6ec7517f139c58d2777602b04a9869c648c1046
havadtoiGabriella <lia.havadtoigabriella@gmail.com>
2022-04-18 13:31:59 +0200

    Colors added

---
+++ auto-tester/week02/arrays/src/com/gfa/havadtoigabriella/Colors.java
@@ -0,0 +1,21 @@
+package com.gfa.havadtoigabriella;
+
+public class Colors {
+  public static void main(String[] args) {
+    // - Create a two dimensional array
+//   which can contain the different shades of specified colors
+// - In `colors[0]` store the shades of green:
+//   `"lime", "forest green", "olive", "pale green", "spring green"`
+// - In `colors[1]` store the shades of red:
+//   `"orange red", "red", "tomato"`
+// - In `colors[2]` store the shades of pink:
+//   `"orchid", "violet", "pink", "hot pink"`
+
+    String[][] colors = {
+        {"lime", "forest green", "olive", "pale green", "spring green"},
+        {"orange red", "red", "tomato"},
+        {"orchid", "violet", "pink", "hot pink"}
+    };
+  }
+}
+
