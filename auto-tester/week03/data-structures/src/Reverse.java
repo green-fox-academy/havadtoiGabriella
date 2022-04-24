9fc292b4b8a29e8ea756f876aba1f023cb8d27b2
havadtoiGabriella <lia.havadtoigabriella@gmail.com>
2022-04-24 18:52:10 +0200

    Reverse added

---
+++ auto-tester/week03/data-structures/src/Reverse.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class Reverse {
+  public static void main(String... args) {
+    String toBeReversed =
+        ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+    // Create a method that can reverse a string which is passed as parameter
+    // Pass the `toBeReversed` variable to this method to check if it works well
+    // At first, solve this task using the `charAt()` function
+    // Try other solutions when you are done
+
+    System.out.println(reverse(toBeReversed));
+  }
+
+  //With char array
+  private static String reverse(String toBeReversed) {
+    char[] charSentence = toBeReversed.toCharArray();
+    char[] newCharSentence = new char[charSentence.length];
+
+    int j = charSentence.length;
+    for (int i = 0; i < charSentence.length; i++) {
+      newCharSentence[i] = charSentence[j - 1];
+      j--;
+    }
+    String reversedSentence = new String(newCharSentence);
+    return reversedSentence;
+  }
+
+  // With Stringbuilder
+  public static String reverse2(String line) {
+    StringBuilder newLine = new StringBuilder(line);
+    int j = line.length();
+    newLine.reverse();
+    return newLine.toString();
+  }
+}
+
