ac1a19d7d023e6ac2ba4cebee6f09c020d1948c0
havadtoiGabriella <lia.havadtoigabriella@gmail.com>
2022-04-28 14:30:05 +0200

    Echo added

---
+++ auto-tester/week03/exception-handling/src/Echo.java
@@ -0,0 +1,23 @@
+public class Echo {
+  /*
+  Create a void function that receives a String as parameter
+It should print the string twice to the console
+If the string is a null it should throw an Excepton with the message:
+the string shouldn't be null
+Call the function with a String
+Call the function with a null
+Handle any raised Exceptions by printing their message to the console
+  echo("Hello");
+  echo(null);
+   */
+  public static void echo(String toPrint) {
+    try {
+      if (toPrint.equals(null)) {
+        throw new NullPointerException();
+      }
+      System.out.println(toPrint + "\n" + toPrint);
+    } catch (NullPointerException ex) {
+      System.err.println("the string shouldn't be null");
+    }
+  }
+}
