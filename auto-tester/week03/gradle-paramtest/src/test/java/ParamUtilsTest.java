b4f3f07f4621c6722702841626eee7c2d12c366d
havadtoiGabriella <lia.havadtoigabriella@gmail.com>
2022-05-01 19:08:47 +0200

    ParamUtils Test modified

--- auto-tester/week03/gradle-paramtest/src/test/java/ParamUtilsTest.java
+++ auto-tester/week03/gradle-paramtest/src/test/java/ParamUtilsTest.java
@@ -1,6 +1,4 @@
 import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
