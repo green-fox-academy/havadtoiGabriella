fd70d67aadbfccc6db49c1b955be69c56e9d50e6
havadtoiGabriella <lia.havadtoigabriella@gmail.com>
2022-05-01 19:07:52 +0200

    ParamTest modified

--- auto-tester/week03/gradle-paramtest/src/test/java/ParameterizedTests.java
+++ auto-tester/week03/gradle-paramtest/src/test/java/ParameterizedTests.java
@@ -1,12 +1,13 @@
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class ParameterizedTest {
+public class ParameterizedTests {
 
   @ParameterizedTest
   @ValueSource(ints = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20})
-  void isEvenShouldReturnTrue(int number){
-
+  void isEvenShouldReturnTrue(int number) {
+    assertTrue(Parameterized.isEven(number));
   }
 }
