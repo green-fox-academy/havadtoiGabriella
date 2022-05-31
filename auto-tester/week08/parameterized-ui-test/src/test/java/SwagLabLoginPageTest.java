48476579fa30bafb2c84d1ad827997db9048d88e
havadtoiGabriella <lia.havadtoigabriella@gmail.com>
2022-05-31 19:23:59 +0200

    SwagLab paramtest added

---
+++ auto-tester/week08/parameterized-ui-test/src/test/java/SwagLabLoginPageTest.java
@@ -0,0 +1,22 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import pages.SwagLabLoginPage;
+
+public class SwagLabLoginPageTest extends BaseTest {
+
+  @ParameterizedTest
+  @CsvFileSource(resources = "source.csv", numLinesToSkip = 1)
+  public void invalidDataLoginTest(String username, String password) {
+    SwagLabLoginPage loginPage = new SwagLabLoginPage(driver);
+    loginPage.load();
+    loginPage.fillUserNameField(username);
+    loginPage.fillPasswordField(password);
+    loginPage.clickLoginButton();
+    assertThat(loginPage.getErrorMessage().getText())
+        .as("With invalid data input, the error message should be: Epic sadface: Username and password do not match any user in this service")
+        .contains("Epic sadface: Username and password do not match any user in this service");
+  }
+
+}
---
+++ auto-tester/week08/parameterized-ui-test/src/test/java/pages/SwagLabLoginPage.java
@@ -0,0 +1,40 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class SwagLabLoginPage extends BasePage{
+  public SwagLabLoginPage(WebDriver driver) {
+    super(driver);
+  }
+
+  @FindBy(id = "user-name")
+  WebElement userNameField;
+  @FindBy(id = "password")
+  WebElement passwordField;
+  @FindBy(id = "login-button")
+  WebElement submitButton;
+  @FindBy(tagName = "h3")
+  WebElement errorMessage;
+
+  public void load(){
+    driver.get("https://www.saucedemo.com/");
+  }
+
+  public void fillUserNameField(String username){
+    userNameField.sendKeys(username);
+  }
+
+  public void fillPasswordField(String password){
+    passwordField.sendKeys(password);
+  }
+
+  public void clickLoginButton(){
+    submitButton.click();
+  }
+
+  public WebElement getErrorMessage(){
+    return errorMessage;
+  }
+}
