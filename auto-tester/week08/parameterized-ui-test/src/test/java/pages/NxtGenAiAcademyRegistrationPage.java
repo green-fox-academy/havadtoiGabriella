e4026d4871a8d7d91723f5ba91aa011d0d80f3a9
havadtoiGabriella <lia.havadtoigabriella@gmail.com>
2022-05-31 20:51:57 +0200

    Next Gen Page modified

---
+++ auto-tester/week08/parameterized-ui-test/src/test/java/pages/NxtGenAiAcademyRegistrationPage.java
@@ -0,0 +1,69 @@
+package pages;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class NxtGenAiAcademyRegistrationPage extends BasePage {
+
+  public NxtGenAiAcademyRegistrationPage(WebDriver driver) {
+    super(driver);
+  }
+
+  @FindBy(id = "vfb-5")
+  WebElement firstNameField;
+  @FindBy(id = "vfb-7")
+  WebElement lastNameField;
+  @FindBy(id = "vfb-8-1")
+  WebElement maleGenderField;
+  @FindBy(id = "vfb-8-2")
+  WebElement femaleGenderField;
+  @FindBy(id = "vfb-3")
+  WebElement verificationField;
+  @FindBy(id = "vfb-14")
+  WebElement emailField;
+  @FindBy(id = "vfb-4")
+  WebElement submitButton;
+
+  public void load() {
+    driver.get("https://nxtgenaiacademy.com/demo-site/");
+  }
+
+  public void fillFirstName(String firstName) {
+    firstNameField.sendKeys(firstName);
+  }
+
+  public void fillLastName(String lastName) {
+    lastNameField.sendKeys(lastName);
+  }
+
+  public void pickGenderRandomly() {
+    getRandomGender().click();
+  }
+
+  public void fillEmailField(String email) {
+    emailField.sendKeys(email);
+  }
+
+  public void fillVerificationField(String verificationNumber) {
+    verificationField.sendKeys(verificationNumber);
+  }
+
+  public SuccessPage clickSubmitButton() {
+    submitButton.click();
+    return new SuccessPage(driver);
+  }
+
+
+  private WebElement getRandomGender() {
+    List<WebElement> list = new ArrayList<>();
+    list.add(maleGenderField);
+    list.add(femaleGenderField);
+    Random r = new Random();
+    return (list.get(r.nextInt(list.size())));
+  }
+
+}
