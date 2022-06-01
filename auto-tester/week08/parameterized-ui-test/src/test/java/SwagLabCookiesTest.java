83bccf869f6c8fa334e404cf2f189b1a4c0135b7
havadtoiGabriella <lia.havadtoigabriella@gmail.com>
2022-06-01 19:27:09 +0200

    First Cookies task added

---
+++ auto-tester/week08/parameterized-ui-test/src/test/java/SwagLabCookiesTest.java
@@ -0,0 +1,27 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Cookie;
+import pages.SwagLabLoginPage;
+
+public class SwagLabCookiesTest extends BaseTest {
+
+  @Test
+  public void deleteSessionCookieTest() {
+    SwagLabLoginPage loginPage = new SwagLabLoginPage(driver);
+    loginPage.load();
+    loginPage.fillUserNameField("standard_user");
+    loginPage.fillPasswordField("secret_sauce");
+    loginPage.clickLoginButton();
+    Set<Cookie> allCookies = driver.manage().getCookies();
+    for (Cookie cookie : allCookies) {
+      System.out.println(cookie.getName());
+    }
+    driver.manage().deleteCookieNamed("session-username");
+    driver.navigate().refresh();
+    assertThat(driver.getCurrentUrl())
+        .as("Browser should be redirected to Login Page, url should be: https://www.saucedemo.com/")
+        .isEqualTo("https://www.saucedemo.com/");
+  }
+}
